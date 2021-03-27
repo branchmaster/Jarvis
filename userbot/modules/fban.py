dd87a89ca7776e54494cc8bee925fea067dde2f9
Nicolás <77582056+nmayorga78@users.noreply.github.com>
2021-03-27 02:41:54 -0300

    revert

--- userbot/modules/fban.py
+++ userbot/modules/fban.py
@@ -1,210 +1,226 @@
-# Copyright (C) 2019 Rupansh Sekar.
+# Copyright (C) 2020 KenHV
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 
-import asyncio
+from sqlalchemy.exc import IntegrityError
 
-from telethon.tl.types import MessageEntityMentionName
-
-from userbot import CMD_HELP, bot, is_mongo_alive, is_redis_alive
-from userbot.events import register, grp_exclude
-from userbot.modules.dbhelper import (add_chat_fban, add_chat_gban, get_fban,
-                                      get_gban, remove_chat_fban,
-                                      remove_chat_gban)
+from userbot import CMD_HELP, bot
+from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.gban")
-@grp_exclude()
-async def gban_all(msg):
-    if not is_mongo_alive() or not is_redis_alive():
-        await msg.edit("`Database connections failing!`")
-        return
-    textx = await msg.get_reply_message()
-    if textx:
-        try:
-            banreason = "[userbot] "
-            banreason += banreason.join(msg.text.split(" ")[1:])
-            if banreason == "[userbot]":
-                raise TypeError
-        except TypeError:
-            banreason = "[userbot] gban"
+@register(outgoing=True, disable_edited=True, pattern=r"^\.fban(?: |$)(.*)")
+async def fban(event):
+    """Bans a user from connected federations."""
+    try:
+        from userbot.modules.sql_helper.fban_sql import get_flist
+    except IntegrityError:
+        return await event.edit("**Running on Non-SQL mode!**")
+
+    if event.is_reply:
+        reply_msg = await event.get_reply_message()
+        fban_id = reply_msg.from_id
+        reason = event.pattern_match.group(1)
+        user_link = f"[{fban_id}](tg://user?id={fban_id})"
     else:
-        banid = msg.text.split(" ")[1]
-        if banid.isnumeric():
-            # if its a user id
-            banid = int(banid)
-        else:
-            # deal wid the usernames
-            if msg.message.entities is not None:
-                probable_user_mention_entity = msg.message.entities[0]
+        pattern = str(event.pattern_match.group(1)).split()
+        fban_id = pattern[0]
+        reason = " ".join(pattern[1:])
+        user_link = fban_id
 
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
-                ban_id = probable_user_mention_entity.user_id
-        try:
-            banreason = "[userbot] "
-            banreason += banreason.join(msg.text.split(" ")[2:])
-            if banreason == "[userbot]":
-                raise TypeError
-        except TypeError:
-            banreason = "[userbot] gban"
-    if not textx:
-        await msg.edit(
-            "Reply Message missing! Might fail on many bots! Still attempting Gban!"
+    self_user = await event.client.get_me()
+
+    if fban_id == self_user.id or fban_id == "@" + self_user.username:
+        return await event.edit(
+            "**Error: This action has been prevented by One4uBot self preservation protocols.**"
         )
-        # Ensure User Read the warning
-        await asyncio.sleep(1)
-    x = (await get_gban())
-    count = 0
-    banlist = []
-    for i in x:
-        banlist.append(i["chatid"])
-    for banbot in banlist:
-        async with bot.conversation(banbot) as conv:
-            if textx:
-                c = await msg.forward_to(banbot)
-                await c.reply("/id")
-            await conv.send_message(f"/gban {banid} {banreason}")
-            resp = await conv.get_response()
-            await bot.send_read_acknowledge(conv.chat_id)
-            count += 1
-            # We cant see if he actually Gbanned. Let this stay for now
-            await msg.edit("`Gbanned on " + str(count) + " bots!`")
-            await asyncio.sleep(0.2)
 
+    if len((fed_list := get_flist())) == 0:
+        return await event.edit("**You haven't connected to any federations yet!**")
 
-@register(outgoing=True, pattern="^.fban")
-@grp_exclude()
-async def fedban_all(msg):
-    if not is_mongo_alive() or not is_redis_alive():
-        await msg.edit("`Database connections failing!`")
-        return
-    textx = await msg.get_reply_message()
-    if textx:
+    await event.edit(f"**Fbanning** {user_link}...")
+    failed = []
+    total = int(0)
+
+    for i in fed_list:
+        total += 1
+        chat = int(i.chat_id)
         try:
-            banreason = "[userbot] "
-            banreason += banreason.join(msg.text.split(" ")[1:])
-            if banreason == "[userbot]":
-                raise TypeError
-        except TypeError:
-            banreason = "[userbot] fban"
-    else:
-        banid = msg.text.split(" ")[1]
-        if banid.isnumeric():
-            # if its a user id
-            banid = int(banid)
-        else:
-            # deal wid the usernames
-            if msg.message.entities is not None:
-                probable_user_mention_entity = msg.message.entities[0]
+            async with bot.conversation(chat) as conv:
+                await conv.send_message(f"/fban {user_link} {reason}")
+                reply = await conv.get_response()
+                await bot.send_read_acknowledge(
+                    conv.chat_id, message=reply, clear_mentions=True
+                )
 
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
-                ban_id = probable_user_mention_entity.user_id
-        try:
-            banreason = "[userbot] "
-            banreason += banreason.join(msg.text.split(" ")[2:])
-            if banreason == "[userbot]":
-                raise TypeError
-        except TypeError:
-            banreason = "[userbot] fban"
-        if "spam" in banreason:
-            spamwatch = True
-        else:
-            spamwatch = False
-    failed = dict()
-    count = 1
-    fbanlist = []
-    x = (await get_fban())
-    for i in x:
-        fbanlist.append(i["chatid"])
-    for bangroup in fbanlist:
+                if (
+                    ("New FedBan" not in reply.text)
+                    and ("Starting a federation ban" not in reply.text)
+                    and ("Start a federation ban" not in reply.text)
+                    and ("FedBan reason updated" not in reply.text)
+                ):
+                    failed.append(i.fed_name)
+        except BaseException:
+            failed.append(i.fed_name)
 
-        # Send to proof to Spamwatch in case it was spam
-        # Spamwatch is a reputed fed fighting against spam on telegram
+    reason = reason if reason else "Not specified."
 
-        if bangroup == -1001312712379:
-            if spamwatch:
-                if textx:
-                    await textx.forward_to(-1001312712379)
-                else:
-                    await msg.reply(
-                        "`Spam message detected. But no reply message, can't forward to SpamWatch.`"
-                    )
-            continue
-        async with bot.conversation(bangroup) as conv:
-            await conv.send_message(f"!fban {banid} {banreason}")
-            resp = await conv.get_response()
-            await bot.send_read_acknowledge(conv.chat_id)
-            if "Beginning federation ban " not in resp.text:
-                failed[bangroup] = str(conv.chat_id)
-            else:
-                count += 1
-                await msg.edit("`Fbanned on " + str(count) + " feds!`")
-            # Sleep to avoid a floodwait.
-            # Prevents floodwait if user is a fedadmin on too many feds
-            await asyncio.sleep(0.2)
     if failed:
-        failedstr = ""
-        for i in failed.keys():
-            failedstr += failed[i]
-            failedstr += " "
-        await msg.reply(f"`Failed to fban in {failedstr}`")
+        status = f"Failed to fban in {len(failed)}/{total} feds.\n"
+        for i in failed:
+            status += "• " + i + "\n"
     else:
-        await msg.reply("`Fbanned in all feds!`")
+        status = f"Success! Fbanned in {total} feds."
+
+    await event.edit(
+        f"**Fbanned **{user_link}!\n**Reason:** {reason}\n**Status:** {status}"
+    )
 
 
-@register(outgoing=True, pattern="^.addfban")
-@grp_exclude()
-async def add_to_fban(chat):
-    if not is_mongo_alive() or not is_redis_alive():
-        await chat.edit("`Database connections failing!`")
-        return
-    await add_chat_fban(chat.chat_id)
-    await chat.edit("`Added this chat under the Fbanlist!`")
+@register(outgoing=True, disable_edited=True, pattern=r"^\.unfban(?: |$)(.*)")
+async def unfban(event):
+    """Unbans a user from connected federations."""
+    try:
+        from userbot.modules.sql_helper.fban_sql import get_flist
+    except IntegrityError:
+        return await event.edit("**Running on Non-SQL mode!**")
+
+    if event.is_reply:
+        reply_msg = await event.get_reply_message()
+        unfban_id = reply_msg.from_id
+        reason = event.pattern_match.group(1)
+        user_link = f"[{unfban_id}](tg://user?id={unfban_id})"
+    else:
+        pattern = str(event.pattern_match.group(1)).split()
+        unfban_id = pattern[0]
+        reason = " ".join(pattern[1:])
+        user_link = unfban_id
+
+    self_user = await event.client.get_me()
+
+    if unfban_id == self_user.id or unfban_id == "@" + self_user.username:
+        return await event.edit("**Wait, that's illegal**")
+
+    if len((fed_list := get_flist())) == 0:
+        return await event.edit("**You haven't connected any federations yet!**")
+
+    await event.edit(f"**Un-fbanning **{user_link}**...**")
+    failed = []
+    total = int(0)
+
+    for i in fed_list:
+        total += 1
+        chat = int(i.chat_id)
+        try:
+            async with bot.conversation(chat) as conv:
+                await conv.send_message(f"/unfban {user_link} {reason}")
+                reply = await conv.get_response()
+                await bot.send_read_acknowledge(
+                    conv.chat_id, message=reply, clear_mentions=True
+                )
+
+                if (
+                    ("New un-FedBan" not in reply.text)
+                    and ("I'll give" not in reply.text)
+                    and ("Un-FedBan" not in reply.text)
+                ):
+                    failed.append(i.fed_name)
+        except BaseException:
+            failed.append(i.fed_name)
+
+    reason = reason if reason else "Not specified."
+
+    if failed:
+        status = f"Failed to un-fban in {len(failed)}/{total} feds.\n"
+        for i in failed:
+            status += "• " + i + "\n"
+    else:
+        status = f"Success! Un-fbanned in {total} feds."
+
+    reason = reason if reason else "Not specified."
+    await event.edit(
+        f"**Un-fbanned** {user_link}!\n**Reason:** {reason}\n**Status:** {status}"
+    )
 
 
-@register(outgoing=True, pattern="^.addgban")
-@grp_exclude()
-async def add_to_gban(chat):
-    if not is_mongo_alive() or not is_redis_alive():
-        await chat.edit("`Database connections failing!`")
-        return
-    await add_chat_gban(chat.chat_id)
-    print(chat.chat_id)
-    await chat.edit("`Added this bot under the Gbanlist!`")
+@register(outgoing=True, pattern=r"^\.addf *(.*)")
+async def addf(event):
+    """Adds current chat to connected federations."""
+    try:
+        from userbot.modules.sql_helper.fban_sql import add_flist
+    except IntegrityError:
+        return await event.edit("**Running on Non-SQL mode!**")
+
+    if not (fed_name := event.pattern_match.group(1)):
+        return await event.edit("**Pass a name in order connect to this group!**")
+
+    try:
+        add_flist(event.chat_id, fed_name)
+    except IntegrityError:
+        return await event.edit(
+            "**This group is already connected to federations list.**"
+        )
+
+    await event.edit("**Added this group to federations list!**")
 
 
-@register(outgoing=True, pattern="^.removefban")
-@grp_exclude()
-async def remove_from_fban(chat):
-    if not is_mongo_alive() or not is_redis_alive():
-        await chat.edit("`Database connections failing!`")
-        return
-    await remove_chat_fban(chat.chat_id)
-    await chat.edit("`Removed this chat from the Fbanlist!`")
+@register(outgoing=True, pattern=r"^\.delf$")
+async def delf(event):
+    """Removes current chat from connected federations."""
+    try:
+        from userbot.modules.sql_helper.fban_sql import del_flist
+    except IntegrityError:
+        return await event.edit("**Running on Non-SQL mode!**")
+
+    del_flist(event.chat_id)
+    await event.edit("**Removed this group from federations list!**")
 
 
-@register(outgoing=True, pattern="^.removegban")
-@grp_exclude()
-async def remove_from_gban(chat):
-    if not is_mongo_alive() or not is_redis_alive():
-        await chat.edit("`Database connections failing!`")
-        return
-    await remove_chat_gban(chat.chat_id)
-    await chat.edit("`Removed this bot from the Gbanlist!`")
+@register(outgoing=True, pattern=r"^\.listf$")
+async def listf(event):
+    """List all connected federations."""
+    try:
+        from userbot.modules.sql_helper.fban_sql import get_flist
+    except IntegrityError:
+        return await event.edit("**Running on Non-SQL mode!**")
+
+    if len((fed_list := get_flist())) == 0:
+        return await event.edit("**You haven't connected to any federations yet!**")
+
+    msg = "**Connected federations:**\n\n"
+
+    for i in fed_list:
+        msg += "• " + str(i.fed_name) + "\n"
+
+    await event.edit(msg)
 
 
-CMD_HELP.update({
-    "fbans/gbans": [
-        'FBans/GBans',
-        " - `.gban`: Reply to a user to ban them in all the bots provided by you.\n"
-        " - `.fban`: Reply to a user to fban them in all the groups provided by you.\n"
-        " - `.addfban`: Add this group to the fbanlist.\n"
-        " - `.addgban`: Add this group to the gbanlist.\n"
-        " - `.removefban`: Remove this group from the fbanlist.\n"
-        " - `.removegban`: Remove this group from the gbanlist.\n"
-    ]
-})
+@register(outgoing=True, disable_edited=True, pattern=r"^\.clearf$")
+async def delf(event):
+    """Removes all chats from connected federations."""
+    try:
+        from userbot.modules.sql_helper.fban_sql import del_flist_all
+    except IntegrityError:
+        return await event.edit("**Running on Non-SQL mode!**")
+
+    del_flist_all()
+    await event.edit("**Disconnected from all connected federations!**")
+
+
+CMD_HELP.update(
+    {
+        "fban": ".fban <id/username> <reason>"
+        "\nUsage: Bans user from connected federations."
+        "\nYou can reply to the user whom you want to fban or manually pass the username/id."
+        "\n\n.unfban <id/username> <reason>"
+        "\nUsage: Same as fban but unbans the user"
+        "\n\n.addf <name>"
+        "\nUsage: Adds current group and stores it as <name> in connected federations."
+        "\nAdding one group is enough for one federation."
+        "\n\n.delf"
+        "\nUsage: Removes current group from connected federations."
+        "\n\n.listf"
+        "\nUsage: Lists all connected federations by specified name."
+    }
+)
