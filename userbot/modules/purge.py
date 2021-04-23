6e750cfc7c1c788f33629ef8c8a2d0fe7c8d0332
Nicolás <77582056+nmayorga78@users.noreply.github.com>
2021-04-22 23:45:51 -0400

    Update purge.py

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -48,9 +48,69 @@ async def fastpurger(purg):
     await sleep(2)
     await done.delete()
 
+    @register(outgoing=True, pattern="^.purgeme")
+async def purgeme(delme):
+    """ For .purgeme, delete x count of your latest message."""
+    message = delme.text
+    count = int(message[9:])
+    i = 1
+
+    async for message in delme.client.iter_messages(delme.chat_id, from_user="me"):
+        if i > count + 1:
+            break
+        i = i + 1
+        await message.delete()
+
+    smsg = await delme.client.send_message(
+        delme.chat_id,
+        "`Purge complete!` Purged " + str(count) + " messages.",
+    )
+    if BOTLOG:
+        await delme.client.send_message(
+            BOTLOG_CHATID, "Purge of " + str(count) + " messages done successfully."
+        )
+    await sleep(2)
+    i = 1
+    await smsg.delete()
+
+
+@register(outgoing=True, pattern="^.del$")
+async def delete_it(delme):
+    """ For .del command, delete the replied message. """
+    msg_src = await delme.get_reply_message()
+    if delme.reply_to_msg_id:
+        try:
+            await msg_src.delete()
+            await delme.delete()
+            if BOTLOG:
+                await delme.client.send_message(
+                    BOTLOG_CHATID, "Deletion of message was successful"
+                )
+        except rpcbaseerrors.BadRequestError:
+            if BOTLOG:
+                await delme.client.send_message(
+                    BOTLOG_CHATID, "Well, I can't delete a message"
+                )
+
+
+    
 CMD_HELP.update(
     {
         "purge": ".purge\
         \nUsage: Purges all messages starting from the reply."
+    }
+)
+
+CMD_HELP.update(
+    {
+        "purgeme": ".purgeme <x>\
+        \nUsage: Elimina x cantidad de sus últimos mensajes."
+    }
+)
+
+CMD_HELP.update(
+    {
+        "del": ".del\
+\nUsage: Elimina el mensaje al que respondiste."
     }
 )
